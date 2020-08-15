52d3ab31cb6fb179306650eeebfeb91e85d3fb86
Jonathan Zufi <jonathan.zufi@equityestatesfund.com>
2020-08-14 21:37:12 -0400

    Initial Commit

---
+++ CustomPickerSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CustomPickerSwiftUI
+//
+//  Created by Jonathan Zufi on 8/14/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CustomPickerSwiftUI/CustomPickerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomPickerSwiftUIApp.swift
+//  CustomPickerSwiftUI
+//
+//  Created by Jonathan Zufi on 8/14/20.
+//
+
+import SwiftUI
+
+@main
+struct CustomPickerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
